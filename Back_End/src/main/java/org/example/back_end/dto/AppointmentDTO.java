160d757d3c8d6ba918040aa84f102b5487af0aeb
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-03-20 21:36:28 +0530

    backend

--- Back_End/src/main/java/org/example/back_end/dto/AppointmentDTO.java
+++ Back_End/src/main/java/org/example/back_end/dto/AppointmentDTO.java
@@ -1,4 +1,17 @@
 package org.example.back_end.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class AppointmentDTO {
+    private int id;
+    private LocalDateTime appointmentDate;
+    private String reason;
+    private String status;
 }
--- Back_End/src/main/java/org/example/back_end/dto/VeterinarianDTO.java
+++ Back_End/src/main/java/org/example/back_end/dto/VeterinarianDTO.java
@@ -1,4 +1,15 @@
 package org.example.back_end.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class VeterinarianDTO {
+    private int id;
+    private String name;
+    private String phone;
+    private String specialty;
 }
