19b1860590798ca100f3df595092ffc493ae37ba
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-03-25 10:50:46 +0530

    mapping tables

--- Back_End/src/main/java/org/example/back_end/dto/AppointmentDTO.java
+++ Back_End/src/main/java/org/example/back_end/dto/AppointmentDTO.java
@@ -1,19 +1,26 @@
 package org.example.back_end.dto;
 
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.back_end.entity.Pet;
+import org.example.back_end.entity.User;
+import org.example.back_end.entity.Veterinarian;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class AppointmentDTO {
     private int id;
-    private LocalDateTime appointmentDate;
+    private Date date;
+    private String time;
     private String reason;
-    private String status;
-    private int veterinarianId;
-    private int petId;
+    private Pet pet;
+    private Veterinarian veterinarian;
+    private User user;
+
 }
--- Back_End/src/main/java/org/example/back_end/dto/MedicalReportDTO.java
+++ Back_End/src/main/java/org/example/back_end/dto/MedicalReportDTO.java
@@ -1,4 +1,19 @@
 package org.example.back_end.dto;
 
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.example.back_end.entity.Pet;
+import org.example.back_end.entity.Veterinarian;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MedicalReportDTO {
+    private int reportId;
+    private String report;
+    private String date;
+    private Pet pet;
+    private Veterinarian veterinarian;
 }
