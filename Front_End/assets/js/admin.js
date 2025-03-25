38645fd034cb74f5dfe1237fb39906aa6a519deb
Sanduni S Priyadarshani <priyadarshanissp@gmail.com>
2025-03-25 09:51:12 +0530

    Admin UI design

--- Front_End/assets/js/admin.js
+++ Front_End/assets/js/admin.js
@@ -4,5 +4,32 @@ $(document).ready(function () {
         window.location.href = 'index.html';
     });
 
+    // load to data for the profile
+ /*   function loadProfile() {
+        let token = localStorage.getItem('token');
 
+        $.ajax({
+            url: "http://localhost:8080/api/v1/user/getProfile",
+            type: "GET",
+            headers: {
+                'Authorization': 'Bearer ' + token
+            },
+            success: function (response) {
+                // $("#userProfileImage").attr("src", "../assets/images/img.png"+response.data.profileImage);
+                $('#adminName').text(response.data.name);
+                //set email to input field
+                $('#email').val(response.data.email);
+                $('#name').val(response.data.name);
+                $('#adminAddress').val(response.data.address);
+                $('#telNo').val(response.data.telNo);
+            },
+            error: function (response) {
+                alert("Something went wrong");
+                localStorage.removeItem('token');
+                window.location.href = 'index.html';
+            }
+        })
+    }
+
+    loadProfile();*/
 });
